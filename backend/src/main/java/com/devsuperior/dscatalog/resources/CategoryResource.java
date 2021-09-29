069b519a494974a6d49ebe2bd43448c2acc2c7c2
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-09-29 09:52:50 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
@@ -31,6 +35,23 @@ public class CategoryResource {
 		CategoryDTO dto = service.findById(id);
 		
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	//por padrão no rest quando se vai inserir um novo recurso utiliza-se o método post e não mais o método get
+	@PostMapping 
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}")
+				.buildAndExpand(dto.getId()).toUri();
+				;
+		
+		return ResponseEntity.created(uri).body(dto);
+		//por padrão o codigo da requisição de um ok() é 200, que significa requisição com sucesso.
+		//mas no caso particular de se criar um novo recurso o codigo http mais correto é o 201 que é de recurso criado.
+		
+		
+		
 	}
 
 }
